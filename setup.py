60e1c6b6722dec4f57a1d767454af0dcaf8697ff
Peter Reutemann <fracpete@gmail.com>
2021-09-14 12:48:05 +1200

    added url, changes/description rest files

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import setup
 
 
+def _read(f):
+    """
+    Reads in the content of the file.
+    :param f: the file to read
+    :type f: str
+    :return: the content
+    :rtype: str
+    """
+    return open(f, 'rb').read()
+
+
 setup(
     name="redit_helper",
     description="Command-line utilities to for sending/receiving data to/from a Redis backend.",
+    long_description=(
+        _read('DESCRIPTION.rst') + b'\n' +
+        _read('CHANGES.rst')).decode('utf-8'),
+    url="https://github.com/fracpete/redis_helper",
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
