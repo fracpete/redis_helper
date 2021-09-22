348dbc1054d9c3f69a7cfa102eb881b81bf6eae3
Peter Reutemann <fracpete@gmail.com>
2021-09-22 15:08:31 +1200

    prepared for release

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     packages=[
       'simple_redis_helper',
     ],
-    version="0.1.0",
+    version="0.1.1",
     author='Peter Reutemann',
     author_email='fracpete@waikato.ac.nz',
     install_requires=[
